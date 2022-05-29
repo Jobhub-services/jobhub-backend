cdb1abac6cc638aa69afb5b89217aec12679544d
ZINEDDINE_KHEDRI <hz_khedri@esi.dz>
2022-05-30 00:11:33 +0100

    Setup update profile function

--- user-service/src/controllers/DeveloperController.ts
+++ user-service/src/controllers/DeveloperController.ts
@@ -1,15 +1,63 @@
 import { Request, Response } from 'express';
 import Developer from '@/models/Developer';
+import { IDeveloper } from '@/interfaces/developer.interface';
+import { DeveloperDto } from '@/dtos/developer.dto';
+import { UploadedFile } from 'express-fileupload';
 class DeveloperController {
 	updateProfile = async (req: Request, res: Response) => {
 		try {
+			const profileBody: DeveloperDto = req.body;
 			const rootObjectId = req.rootObjectId;
 			const profile = await Developer.findOne({ userId: rootObjectId });
+
+			if (profile.summary) this.setSummary(profile, profileBody.summary);
+			if (profile.languages) this.setLanguages(profile, profileBody.languages);
+			if (profile.skills) this.setSkills(profile, profileBody.skills);
+			if (profile.role) this.setRole(profile, profileBody.role);
+			if (profile.work_experience) this.setExperiences(profile, profileBody.work_experience);
+			if (profile.educations) this.setEducations(profile, profileBody.educations);
+			if (profile.certifications) this.setCertifications(profile, profileBody.certifications);
+			if (profile.social_profile) this.setSocialProfile(profile, profileBody.social_profile);
+			if (req.files && req.files.resume) await this.updateResume(profile, req.files.resume as UploadedFile);
+
+			await profile.save();
 			res.status(200).send({ content: profile });
 		} catch {
 			res.status(500).send({ message: 'Something went wrong please try again' });
 		}
 	};
-	private setResume = () => {};
+	private setSummary = (profile: IDeveloper, summary: IDeveloper['summary']) => {
+		profile.summary = summary;
+	};
+
+	private setLanguages = (profile: IDeveloper, languages: IDeveloper['languages']) => {
+		profile.languages = languages;
+	};
+
+	private setSkills = (profile: IDeveloper, skills: IDeveloper['skills']) => {
+		profile.skills = skills;
+	};
+
+	private setRole = (profile: IDeveloper, role: IDeveloper['role']) => {
+		profile.role = role;
+	};
+
+	private setExperiences = (profile: IDeveloper, experiences: IDeveloper['work_experience']) => {
+		profile.work_experience = experiences;
+	};
+
+	private setEducations = (profile: IDeveloper, educations: IDeveloper['educations']) => {
+		profile.educations = educations;
+	};
+
+	private setCertifications = (profile: IDeveloper, certifications: IDeveloper['certifications']) => {
+		profile.certifications = certifications;
+	};
+
+	private setSocialProfile = (profile: IDeveloper, socialProfile: IDeveloper['social_profile']) => {
+		profile.social_profile = socialProfile;
+	};
+
+	private updateResume = async (profile: IDeveloper, file: UploadedFile) => {};
 }
 export default DeveloperController;
--- user-service/src/dtos/developer.dto.ts
+++ user-service/src/dtos/developer.dto.ts
@@ -1,8 +1,7 @@
 import { Schema } from 'mongoose';
-import { IsArray, IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, ValidateNested } from 'class-validator';
-import { UserType } from '@/interfaces/users.interface';
+import { IsArray, IsEnum, IsOptional, IsString, ValidateNested } from 'class-validator';
 import { JobTypes } from '@/interfaces/companyJob.interface';
-import { isEmptyMessage, isStringMessage, isEnumMessage } from '@/config/dto.config';
+import { isStringMessage, isEnumMessage } from '@/config/dto.config';
 import { Expose, Type } from 'class-transformer';
 import { IsObjectId, IsExists } from '@/helpers';
 import Language from '@/models/Language';
@@ -66,11 +65,86 @@ class ExperiencesDto {
 	location: Schema.Types.ObjectId;
 }
 
-class EducationsDto {}
+class EducationsDto {
+	@Expose()
+	@IsOptional()
+	@IsString()
+	title: string;
 
-class CertificationsDto {}
+	@Expose()
+	@IsOptional()
+	@IsString()
+	school: string;
 
-class SocialsDto {}
+	@Expose()
+	@IsOptional()
+	@IsString()
+	startDate: string;
+
+	@Expose()
+	@IsOptional()
+	@IsString()
+	endDate: string;
+}
+
+class CertificationsDto {
+	@Expose()
+	@IsOptional()
+	@IsString()
+	certificationId: string;
+
+	@Expose()
+	@IsOptional()
+	@IsString()
+	title: string;
+
+	@Expose()
+	@IsOptional()
+	@IsString()
+	provider: string;
+
+	@Expose()
+	@IsOptional()
+	@IsString()
+	description: string;
+
+	@Expose()
+	@IsOptional()
+	@IsString()
+	issuedDate: string;
+
+	@Expose()
+	@IsOptional()
+	@IsString()
+	expirationDate: string;
+
+	@Expose()
+	@IsOptional()
+	@IsString()
+	link: string;
+}
+
+class SocialsDto {
+	@Expose()
+	@IsOptional()
+	@IsString()
+	website: string;
+
+	@Expose()
+	@IsOptional()
+	@IsString()
+	git: string;
+
+	@Expose()
+	@IsOptional()
+	@IsString()
+	linkedin: string;
+
+	@Expose()
+	@IsOptional()
+	@IsString()
+	twitter: string;
+}
 
 export class DeveloperDto {
 	@Expose()
@@ -88,7 +162,7 @@ export class DeveloperDto {
 	@Expose()
 	@IsOptional()
 	@IsObjectId({ each: true })
-	skills: string[];
+	skills: Schema.Types.ObjectId[];
 
 	@Expose()
 	@IsOptional()
@@ -105,12 +179,14 @@ export class DeveloperDto {
 
 	@Expose()
 	@IsOptional()
+	@IsArray()
 	@ValidateNested({ each: true })
 	@Type(() => EducationsDto)
 	educations: EducationsDto[];
 
 	@Expose()
 	@IsOptional()
+	@IsArray()
 	@ValidateNested({ each: true })
 	@Type(() => CertificationsDto)
 	certifications: CertificationsDto[];
