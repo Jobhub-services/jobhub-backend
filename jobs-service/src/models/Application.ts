993b0e3ccd5296c8a4654fb3b7ee947e8c38b8a7
khedri-zineddine <hz_khedri@esi.dz>
2022-06-24 21:13:14 +0100

    add normilize function

--- jobs-service/src/models/Application.ts
+++ jobs-service/src/models/Application.ts
@@ -27,5 +27,37 @@ const applicationSchema: Schema = new Schema(
 )
 
 const Company = softDeleteModel<IApplication & Document>('Application', applicationSchema);
+export const normalizetoJSON = (object: any, includeQuestion: boolean = false) => {
+    const job = object.toJSON();
+    return {
+        ...job,
+        category: job.category?.name,
+        company_division: job.company_division?.name,
+        currency: {
+            code: job.currency?.code,
+            name: job.currency?.name,
+        },
+        skills: job.skills?.map((skill) => {
+            return skill.name;
+        }),
 
+        questions: job.questions?.map((question) => {
+            if (includeQuestion) return { _id: question._id, question: question.question }
+            return question.question;
+        }),
+        work_location: {
+            country: job.work_location?.country.name,
+            city: job.work_location?.city,
+        },
+        hire_location: job.hire_location?.map((location) => {
+            return {
+                country: location?.country.name,
+                city: location?.city,
+            };
+        }),
+        createdBy: {
+            companyName: job?.createdBy?.companyInfo?.companyName
+        }
+    };
+};
 export default Company;
