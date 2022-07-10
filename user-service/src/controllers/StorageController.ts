c806313a0f7acfec9c08133a0e5d60e460ff8313
KHEDRI ZINEDDINE <hz_khedri@esi.dz>
2022-07-10 18:18:55 +0100

    migrate to mega storage

--- user-service/src/controllers/StorageController.ts
+++ user-service/src/controllers/StorageController.ts
@@ -1,13 +1,17 @@
 import { storageService } from '@/services/StorageService';
+import * as mime from 'mime-types';
 import { Request, Response } from 'express';
 
 class StorageController {
 	resolveFile = async (req: Request, res: Response) => {
 		try {
 			const fileToken = req.params.token;
-			const filePath = storageService.readFileFromUrl(fileToken);
-			res.download(filePath);
+			const [fileName, fileData] = await storageService.readFileFromUrl(fileToken);
+			const contentType = mime.lookup(fileName) as string;
+			res.setHeader('content-type', contentType);
+			res.send(fileData);
 		} catch (e) {
+			console.log(e);
 			res.status(404).send({ message: 'File not found' });
 		}
 	};
--- user-service/src/services/StorageService.ts
+++ user-service/src/services/StorageService.ts
@@ -1,19 +1,31 @@
+import { Storage } from 'megajs';
 import { STORAGE_API_PATH, STORAGE_PATH } from '@/constants/app.constants';
 import { UploadedFile } from 'express-fileupload';
 import path from 'path';
 import fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
 import { tokenService } from './HashService';
+
 class StorageService {
 	appUrl = process.env.APP_URL;
+	get storage() {
+		return new Storage({
+			email: 'zineddine@aimngt.com',
+			password: '123456789zino',
+		}).ready;
+	}
 	async moveFile(file: UploadedFile) {
-		const extname = path.extname(file.name);
+		const fileName = `${authUser._id}_${uuidv4()}_${file.name}`;
+		const storage = await this.storage;
+		await storage.upload(fileName, file.data).complete;
+		return fileName;
+		/*const extname = path.extname(file.name);
 		const fileName = `${authUser._id}/${uuidv4()}${extname}`;
 		const folderPath = path.resolve(__dirname, '../..', `${STORAGE_PATH}/${authUser._id}`);
 		if (!fs.existsSync(folderPath)) fs.mkdirSync(folderPath, { recursive: true });
 		const filePath = path.resolve(__dirname, '../..', STORAGE_PATH, fileName);
 		await file.mv(filePath);
-		return fileName;
+		return fileName;*/
 	}
 	createFileURL = (fileName: string, expiresIn = 60 * 60 * 24) => {
 		const appUrl = this.appUrl;
@@ -21,11 +33,16 @@ class StorageService {
 		const fileURL = `${appUrl}${STORAGE_API_PATH}/${token}`;
 		return fileURL;
 	};
-	readFileFromUrl = (token: string) => {
+	readFileFromUrl = async (token: string) => {
 		const payload = tokenService.verifyToken(token);
-		const fileName = `${STORAGE_PATH}/${payload.fileName}`;
-		const filePath = path.resolve(__dirname, '..', '..', fileName);
-		return filePath;
+		const fileName = payload.fileName;
+		const storage = await this.storage;
+		const file = storage.root.children.find((item) => item.name === fileName);
+		//@ts-ignore
+		const data = await file.downloadBuffer();
+		return [fileName, data];
+		/*const filePath = path.resolve(__dirname, '..', '..', STORAGE_PATH, fileName);
+		return filePath;*/
 	};
 }
 export const storageService = new StorageService();
